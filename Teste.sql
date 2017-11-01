7c67610650e0a2faa3d87e8190f5af871699b8cc
Breno Aquino <brenoaquinop7@gmail.com>
2017-11-01 11:57:58 -0200

    Adição dos triggers.

--- Teste.sql
+++ Teste.sql
@@ -1,7 +1,22 @@
 insert into clientes values (0, "Breno", "Fortaleza");
+
 insert into produtos values (0, "Produto Teste", 10.0, 15.0, 20, 5);
 
 insert into vendas values (0, CURDATE(), CURDATE(), true, 0);
-insert into itensVendidos values (0, 0, 2, 20.0);
+
+insert into itensVendidos values (0, 0, 20, 20.0);
+
+
+delete from itensVendidos where codigoProduto = 0 and numeroVenda = 0;
 
 delete from produtos where codigo = 0;
+
+
+
+select * from clientes;
+
+select * from produtos;
+
+select * from vendas;
+
+select * from itensVendidos;
---
+++ Triggers.sql
@@ -0,0 +1,51 @@
+use VendasBD;
+
+drop trigger verifica_estoque
+
+delimiter $$
+create trigger verifica_estoque before insert on itensVendidos
+for each row
+begin
+
+	declare qtd int(10);
+    
+    set qtd = (select quantidadeEst from produtos where codigo = new.codigoProduto);
+    
+    if (qtd - new.qtdVenda) < 0 then
+		
+        signal sqlstate '45000' set message_text = 'Estoque insuficiente.';
+
+	else 
+    
+		update produtos set quantidadeEst = (qtd - new.qtdVenda) where codigo = new.codigoProduto;
+
+	end if;
+
+end $$
+delimiter ;
+
+drop trigger att_date
+
+delimiter $$
+create trigger att_date before insert on vendas
+for each row
+begin
+
+	set new.dataVenda = curdate();
+
+	if new.isPrazo then
+		
+        if new.dataVencimento < new.dataVenda then
+			signal sqlstate '45000' set message_text = 'Data de vencimento invalida.';
+		end if;
+    
+    else
+    
+		set new.dataVencimento = curdate();
+        
+		set new.codigoCliente = -1;
+        
+    end if;
+
+end $$
+delimiter ;
